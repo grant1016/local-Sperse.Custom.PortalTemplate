b4b5c60d43cd816ea97bce9cf1824a0e6e01706e
Oleh Stakhiv <os3@sperse.com>
2020-09-30 17:09:55 +0300

    fixed styles for copy icon, changed resellers to reseller-info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ import { AppComponent } from './app.component';
                         data: { localizationSource: AppConsts.localization.CRMLocalizationSourceName }
                     },
                     {
-                        path: 'resellers',
+                        path: 'reseller-info',
                         loadChildren: () => import('shared/common/referral/referral.module').then(m => m.ReferralModule)
                     }
                 ]
