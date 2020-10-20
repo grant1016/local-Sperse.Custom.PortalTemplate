c147321e9fc7dea381fb5a56e60c50d00aa5c664
DenysTkachuk <denys.tkachuk@sperse.com>
2020-10-20 12:28:14 +0300

    [SC-13235] updated general form and account pages styles, completed login page, updated responsive styles

--- src/account/login/login.component.ts
+++ src/account/login/login.component.ts
@@ -22,15 +22,11 @@ import { SettingService } from '@abp/settings/setting.service';
 import { AppSessionService } from '@shared/common/session/app-session.service';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 
-export class AdLoginHostDirective {
-    constructor(public viewContainerRef: ViewContainerRef) { }
-}
 
 @Component({
     templateUrl: './login.component.html',
     styleUrls: [ './login.component.less' ],
-    animations: [accountModuleAnimation()],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    animations: [accountModuleAnimation()]
 })
 export class LoginComponent implements OnInit {
     @ViewChild('loginForm', { static: false }) loginForm;
