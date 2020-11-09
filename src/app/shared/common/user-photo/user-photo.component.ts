faf0247ebca4bde9e7d729b0b5f5d0516de1cf97
DenysTkachuk <denys.tkachuk@sperse.com>
2020-11-09 17:16:07 +0200

    [SC-13415] applied review comments

--- src/app/shared/common/user-photo/user-photo.component.ts
+++ src/app/shared/common/user-photo/user-photo.component.ts
@@ -1,24 +1,32 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+import { takeUntil } from 'rxjs/operators';
+
 import { ProfileService } from '@shared/common/profile-service/profile.service';
 import { UserManagementService } from '@shared/common/layout/user-management-list/user-management.service';
+import { LifecycleSubjectsService } from '@shared/common/lifecycle-subjects/lifecycle-subjects.service';
 
 @Component({
     selector: 'user-photo',
     templateUrl: 'user-photo.component.html',
     styleUrls: [ 'user-photo.component.less' ],
+    providers: [ LifecycleSubjectsService ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserPhotoComponent {
     @Input() uploadAfterSave = true;
     @Input() profilePictureUrl: string;
+
     constructor(
         private profileService: ProfileService,
-        private userManagementService: UserManagementService
+        private userManagementService: UserManagementService,
+        private lifeCycleSubjectService: LifecycleSubjectsService
     ) {}
 
     changeProfilePicture(e: MouseEvent) {
-        this.userManagementService.changeProfilePicture(e, this.uploadAfterSave).subscribe((base64OrigImage: string) => {
+        this.userManagementService.changeProfilePicture(e, this.uploadAfterSave).pipe(
+            takeUntil(this.lifeCycleSubjectService.destroy$)
+        ).subscribe((base64OrigImage: string) => {
             this.profileService.updatePictureUrl(base64OrigImage
                 ? this.profileService.getPhoto(base64OrigImage)
                 : this.profileService.getProfilePictureUrl(null));
