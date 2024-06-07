5fcf1eec9739a1b1b32d911a0ceddf7212f8c68b
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2024-06-07 16:46:59 +0300

    [SC-17729] Moved adding js/css urls from RootComponent to DomHelper

--- src/shared/helpers/DomHelper.ts
+++ src/shared/helpers/DomHelper.ts
@@ -1,3 +1,5 @@
+import * as _ from 'underscore';
+
 export class DomHelper {
 
     static waitUntilElementIsReady(selector: string, callback: any, checkPeriod?: number): void {
@@ -38,4 +40,34 @@ export class DomHelper {
         }, checkPeriod);
     }
 
+    public static addScriptLink(src: string, type: string = 'text/javascript', callback = null, data = {}): void {
+        if (Array.prototype.some.call(document.scripts, (script) => {
+            return script.src == src;
+        })) return ;
+
+        let script = document.createElement('script');
+        script.type = type;
+        script.src = src;
+        for (const prop in data)
+            script.dataset[prop] = data[prop];
+
+        if (callback)
+            script.addEventListener('load', callback);
+        document.head.append(script);
+    }
+
+    public static removeScriptLink(src: string): void {
+        let script = document.querySelector('script[src="' + src + '"]');
+        if (script) script.remove();
+    }
+
+    public static addStyleSheet(id: string, href: string, rel: string = 'stylesheet'): void {
+        let link = document.createElement('link');
+         _.mapObject({id: id, href: href, rel: rel},
+             (val, key) => {
+                 link.setAttribute(key, val);
+             }
+        );
+        document.head.append(link);
+    }
 }
